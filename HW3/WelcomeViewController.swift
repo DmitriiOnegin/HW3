05e8ab1590654a67bacc32279e8f24bd067be21d
Dmitrii Onegin <dimimimo@mail.ru>
2021-12-15 23:10:10 +0300

    add Navigation Controller

---
+++ HW3/WelcomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeViewController.swift
+//  HW3
+//
+//  Created by Dmitrii Onegin on 15.12.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
