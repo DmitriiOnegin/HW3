fb953c93983dce36aad92f7583723e5f5de1dc1b
Dmitrii Onegin <dimimimo@mail.ru>
2021-12-15 23:24:29 +0300

    add sigwey

--- HW3/LoginViewController.swift
+++ HW3/LoginViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+    
+    @IBAction func logInAction() {
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        guard let welcomeVC = segue.source as? WelcomeViewController else { return }
+        
+    }
 }
 
--- HW3/WelcomeViewController.swift
+++ HW3/WelcomeViewController.swift
@@ -8,11 +8,18 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
+    
+    @IBOutlet weak var welcomeTextField: UILabel!
+    
+    var userName: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
-
+    @IBAction func logOutAction() {
+        dismiss(animated: true)
+    }
+    
 }
