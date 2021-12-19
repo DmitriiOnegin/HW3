ad417cabd3e169465c4297896840316e97e57499
Dmitrii Onegin <dimimimo@mail.ru>
2021-12-19 09:53:59 +0300

    Add folders

---
+++ HW3/Models/Person.swift
@@ -0,0 +1,8 @@
+//
+//  Person.swift
+//  HW3
+//
+//  Created by Dmitrii Onegin on 19.12.2021.
+//
+
+import Foundation
---
+++ HW3/Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  HW3
+//
+//  Created by Dmitrii Onegin on 19.12.2021.
+//
+
+import Foundation
